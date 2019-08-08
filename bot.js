d9456a0ff708de4db0e5d9f9cb3fa9aacd6213d9
ツThe Watchers Bot Devsツ <47341267+GrimDesignsFiveM@users.noreply.github.com>
2019-08-08 03:37:04 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -57,13 +57,13 @@ bot.on("message", async message => {
 	let sEmbed2 = new Discord.RichEmbed()
    .setTitle("How To Stop The Spam :speak_no_evil:")
     .setColor("#0x3dfbff")
-   .setDescription('Are you sure you want to shut me down?\n\nReply with \`cancel\` to **abort** the shutdown. The shutdown will self-abort in 30 seconds.')
+   .setDescription('Are you sure you want to shut me down?\n\nReply with \`cancel\` to **abort** the shutdown. The shutdown will self-abort in 60 seconds.')
    .addField("How To Stop The Bot", "Reply with ``yes``, ``y`` for Yes or ``no``, ``n`` for No")  
 	
 	let sEmbed3 = new Discord.RichEmbed()
    .setTitle("How To Stop The Spam :speak_no_evil:")
     .setColor("#0x3dfbff")
-   .setDescription(`@${message.author.id}>` + "Reply with ``yes`` or ``no`` if you need more help check your DMs this process will expire in ``60 seconds``")
+   .setDescription(`<@${message.author.id}>` + " Reply with ``yes`` or ``no`` if you need more help check your DMs The shutdown will self-abort in ``60 seconds.``")
    
    
   message.channel.send(sEmbed3)
