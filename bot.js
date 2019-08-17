2ddd10235865a752fe62e067dbd8b604c71cfea8
ツThe Watchers Bot Devsツ <47341267+GrimDesignsFiveM@users.noreply.github.com>
2019-08-17 02:40:20 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,7 +16,7 @@ const prefix = ";";
 const bot = new Discord.Client({disableEveryone: true});
 
 bot.on("ready", async () => {
-    bot.user.setGame(`Playing ;help with knives!`, `https://twitch.tv/monstercat`)
+    bot.user.setGame(`Playing with knives!`, `https://twitch.tv/monstercat`)
 
 
     console.log(`${BotName} Loaded!`);
