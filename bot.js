a2218737b4fb5372c3b13a060e1d8b25102ec875
ツThe Watchers Bot Devsツ <47341267+GrimDesignsFiveM@users.noreply.github.com>
2019-08-17 01:23:06 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,18 +15,8 @@ const prefix = ";";
 
 const bot = new Discord.Client({disableEveryone: true});
 
-const activities_list = [
-    "With the ;help command.", 
-    "With knives!",
-    "Server destruction", 
-    "Don't hate me"
-    ]; // creates an arraylist containing phrases you want your bot to switch through.
-
-client.on('ready', () => {
-    setInterval(() => {
-        const index = Math.floor(Math.random() * (activities_list.length - 1) + 1); // generates a random number between 1 and the length of the activities array list (in this case 5).
-        client.user.setActivity(activities_list[index]); // sets bot's activities to one of the phrases in the arraylist.
-    }, 10000); // Runs this every 10 seconds.
+bot.on("ready", async () => {
+    bot.user.setGame(`Playing ;help with knives!`, `https://twitch.tv/monstercat`)
 
 
     console.log(`${BotName} Loaded!`);
